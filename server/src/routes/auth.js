d741c14754da5d92ca9236d6f81691cd36e7c60a
Botik123 <x0bar@yandex.ru>
2026-06-28 00:32:53 +1000

    fix: добавлен authMiddleware в импорты

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
 const db = require('../db/queries');
-const { generateToken, generateRefreshToken } = require('../middleware/auth');
+const { generateToken, generateRefreshToken, authMiddleware } = require('../middleware/auth');
 const { sanitizeBody } = require('../middleware/sanitize');
 
 const router = express.Router();
